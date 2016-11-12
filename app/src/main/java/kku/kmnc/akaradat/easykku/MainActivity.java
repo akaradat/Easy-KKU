bf4d09032878bcaeb72c97e8f524c7e980039639
Array <Akrd.Kmnc@kkumail.com>
2016-11-12 12:04:46 +0700

    Intent to SignUp finish

--- app/src/main/java/kku/kmnc/akaradat/easykku/MainActivity.java
+++ app/src/main/java/kku/kmnc/akaradat/easykku/MainActivity.java
@@ -1,14 +1,39 @@
 package kku.kmnc.akaradat.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-}
+        //Bind Widget คือ ผูกวิตเก็ตกับตัวแปร
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //Sign Up Controller ทำให้ปุ่มกดเปลี่ยนหน้าได้
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+
+
+    }   // Main method
+}   // Main Class นี่คือ คลาสหลัก
