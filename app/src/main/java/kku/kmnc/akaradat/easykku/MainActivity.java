a3a26090164e7f79f97580cbf7ffdf6862ab35a9
Array <Akrd.Kmnc@kkumail.com>
2016-11-13 12:14:05 +0700

    Read JSON finish

--- app/src/main/java/kku/kmnc/akaradat/easykku/MainActivity.java
+++ app/src/main/java/kku/kmnc/akaradat/easykku/MainActivity.java
@@ -1,17 +1,26 @@
 package kku.kmnc.akaradat.easykku;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private Button signInButton, signUpButton;
-
-
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+    private MyConstant myConstant;
 
 
 
@@ -20,11 +29,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Bind Widget คือ ผูกวิตเก็ตกับตัวแปร
+        myConstant = new MyConstant();
+
+        //Bind Widget
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
 
-        //Sign Up Controller ทำให้ปุ่มกดเปลี่ยนหน้าได้
+        //Sign In Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value from Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(MainActivity.this, R.drawable.bird48,
+                            getResources().getString(R.string.title_haveSpace),
+                            getResources().getString(R.string.message_HaveSpace));
+                    myAlert.MyDialog();
+                } else {
+                    //No Space
+                    SynUser synUser = new SynUser(MainActivity.this);
+                    synUser.execute(myConstant.getUrlGetJSON());
+
+                }
+
+            }   // onClick
+        });
+
+
+        //Sign Up Controller
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,7 +73,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }    // Main Method
 
 
-    }   // Main method
+    private class SynUser extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+
+        public SynUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("13novV2", "e doIn ==> " + e.toString());
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("13novV2", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // SynUser
+
+
+
+
 }   // Main Class นี่คือ คลาสหลัก
